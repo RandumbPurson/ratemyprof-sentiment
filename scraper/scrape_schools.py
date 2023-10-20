98a2e4f31e969f8ea93e94ec48c79c4053b5ff2c
Emma Fountain <randumbemma@gmail.com>
2023-10-20 00:43:18 -0400

    Fix headless arg

--- scraper/scrape_schools.py
+++ scraper/scrape_schools.py
@@ -7,14 +7,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("schoolID")
 parser.add_argument("-o", help="The output filename, should not include extension")
 parser.add_argument("-s", default=500, type=int, help="The max number of scroll iterations")
-parser.add_argument("--headless", default=True, help="Whether to run the chromedriver in headless mode")
+parser.add_argument("--headless", default=1, help="Whether to run the chromedriver in headless mode1 for headless, 0 for not headless")
 
 args = parser.parse_args()
+
+headless = args.headless == 1
 
 def scrape_schools():
     scraper = RMPscraper(
         base=f"https://www.ratemyprofessors.com/search/professors/{args.schoolID}?q=*",
-        headless=args.headless
+        headless=headless
     )
     scraper.find_click(".CCPAModal__StyledCloseButton-sc-10x9kq-2") # Closes cookies popup
     scraper.expand_all(max_iters=args.s)
